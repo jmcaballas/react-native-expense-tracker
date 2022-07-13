8edb2c8697581cc912b6cdeee367dc396eebfebe
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-07-13 21:38:50 +0800

    Create date input field for CashFlowCreate

--- components/CashFlowCreate.tsx
+++ components/CashFlowCreate.tsx
@@ -1,9 +1,26 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useState } from "react";
 
 import { StatusBar } from "expo-status-bar";
 
+import DateTimePicker from "@react-native-community/datetimepicker";
+
 const CashFlowCreate = () => {
+  const [date, setDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const onChange = (event: any, selectedDate: any) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+    setShowDatePicker(false);
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -27,11 +44,15 @@ const CashFlowCreate = () => {
         placeholder="Category"
         placeholderTextColor="#777e90"
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Date & Time"
-        placeholderTextColor="#777e90"
-      />
+      <TouchableOpacity
+        style={styles.datePicker}
+        onPress={() => setShowDatePicker(true)}
+      >
+        <Text style={styles.dateText}>{date.toString().slice(4, 15)}</Text>
+      </TouchableOpacity>
+      {showDatePicker && (
+        <DateTimePicker value={date} mode={"date"} onChange={onChange} />
+      )}
       <StatusBar style="light" />
     </View>
   );
@@ -52,6 +73,19 @@ const styles = StyleSheet.create({
     marginTop: 20,
     paddingLeft: 15,
     paddingVertical: 10,
+  },
+  datePicker: {
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "#1b2036",
+    marginHorizontal: 20,
+    marginTop: 20,
+    paddingLeft: 15,
+    paddingVertical: 15,
+  },
+  dateText: {
+    color: "white",
   },
 });
 
