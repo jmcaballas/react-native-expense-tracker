e4e34bddb41cfe0d9644619be13958790d1e8145
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-07-13 20:20:03 +0800

    Create input fields for CashFlowCreate

--- components/CashFlowCreate.tsx
+++ components/CashFlowCreate.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useState } from "react";
 
 import { StatusBar } from "expo-status-bar";
@@ -6,7 +6,32 @@ import { StatusBar } from "expo-status-bar";
 const CashFlowCreate = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Expense Tracker</Text>
+      <TextInput
+        keyboardType="number-pad"
+        style={styles.input}
+        placeholder="Amount"
+        placeholderTextColor="#777e90"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        placeholderTextColor="#777e90"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Description"
+        placeholderTextColor="#777e90"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Category"
+        placeholderTextColor="#777e90"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Date & Time"
+        placeholderTextColor="#777e90"
+      />
       <StatusBar style="light" />
     </View>
   );
@@ -16,11 +41,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#232a44",
-    alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
+  input: {
     color: "white",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "#1b2036",
+    marginHorizontal: 20,
+    marginTop: 20,
+    paddingLeft: 15,
+    paddingVertical: 10,
   },
 });
 
